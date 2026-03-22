83d3492c7a531df313200ab7e088f4930d01f057
jaimeviloriogreen <jaimeviloriogreen@hotmail.com>
2026-03-22 16:31:06 -0400

    create, list and delete

--- clase-siete/src/main.js
+++ clase-siete/src/main.js
@@ -10,8 +10,30 @@ const table = document.querySelector(".data-table");
 const tbody = table.querySelector(".tbody");
 const thead = table.querySelector(".thead");
 
+const form = document.querySelector(".form-data");
+let showTable = false;
+
 // Funciones
-button.addEventListener("click", async () => {
+async function submitForm(e) {
+  e.preventDefault();
+  const fname = form.fname.value;
+  const lname = form.lname.value;
+  const career = form.career.value;
+
+  const { error } = await supabase.from("students").insert({ fname, lname, career });
+
+  if (!error) {
+    console.log({ error });
+    console.log({ fname, lname, career });
+    if (showTable) renderTable();
+  }
+
+  // Limpia el formulario
+  form.reset();
+  // Envía el focus al input fname
+  form.fname.focus();
+}
+async function renderTable() {
   const { data, error } = await supabase.from("students").select("*");
   tbody.innerHTML = "";
 
@@ -21,6 +43,7 @@ button.addEventListener("click", async () => {
         <th>Nombre</th>
         <th>Apellido</th>
         <th>Carrera</th>
+        <th>Acción</th>
       </tr>
   `;
 
@@ -28,12 +51,42 @@ button.addEventListener("click", async () => {
     data.forEach((student) => {
       tbody.innerHTML += /*html*/ `
         <tr>
-          <td data-uuid=${student.uuid}> ${student.id} </td>
+          <td> ${student.id} </td>
           <td> ${student.fname} </td>
           <td> ${student.lname} </td>
           <td> ${student.career}</td>
+          <td>
+              <button class="action delete" data-uuid=${student.uuid}>Delete</button>
+              <button class="action update" data-uuid=${student.uuid}>Update</button>
+          </td>
         </tr>
       `;
     });
+
+    showTable = true;
   }
-});
+}
+
+async function deleteStudent(e) {
+  // const button = e.target;
+
+  // El botón tiene dos clases, action y delete, por lo que delete tiene el ìndice 1
+  if (e.target.classList[1] === "delete") {
+    const uuid = e.target.dataset.uuid;
+    if (uuid) {
+      const { data, error } = await supabase.from("students").delete().eq("uuid", uuid);
+
+      // button.textContent = "Loading...";
+
+      if (!error) {
+        renderTable();
+        console.info(`Usuario con el uuid: ${uuid} ha sido eliminado.`);
+      }
+    }
+  }
+}
+
+// Event lister
+button.addEventListener("click", renderTable);
+form.addEventListener("submit", submitForm);
+tbody.addEventListener("click", deleteStudent);
